804dfe6198814eef81b0ab3efe8ad6ca0b9e4530
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-04-10 16:05:35 +0800

    Delete wmm/3rd directory

--- wmm/3rd/download/yyjson-0.4.0/test/compile_ansi.cpp
+++
@@ -1,5 +0,0 @@
-#include "yyjson.c"
-
-int main(void) {
-    return !!yyjson_read(" ", 1, 0);
-}
--- wmm/3rd/install/MSVC/rapidjson-1.1.0/Debug/cmake/RapidJSONConfig.cmake
+++
@@ -1,3 +0,0 @@
-get_filename_component(RAPIDJSON_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-set(RAPIDJSON_INCLUDE_DIRS "D:/UserData/repos/writing-assistant/writing_assistant/3rd/install/rapidjson-1.1.0/Debug/include")
-message(STATUS "RapidJSON found. Headers: ${RAPIDJSON_INCLUDE_DIRS}")
