3ee35c60c6d11d4a44b65145fa2c07fb39dbedf1
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-04-10 19:45:25 +0800

    Update DataSourceManager.cpp

--- wmm/src/DataSourceManager.cpp
+++ wmm/src/DataSourceManager.cpp
@@ -1,10 +1,17 @@
 #include "DataSourceManager.h"
 #include "ui_DataSourceManager.h"
 
-using namespace WritingMaterialsManager;
+#include <QGridLayout>
 
-DataSourceManager::DataSourceManager(QWidget* parent) : QMainWindow(parent), ui(new Ui::DataSourceManager) {
+namespace WMM = WritingMaterialsManager;
+using namespace WMM;
+
+DataSourceManager::DataSourceManager(QWidget* parent) : QMainWindow(parent), ui(new WMM::Ui::DataSourceManager) {
     ui->setupUi(this);
+
+    // preparation
+    centralWidget()->setLayout(new QGridLayout);
+    centralWidget()->layout()->addWidget(ui->DataSourceTab);
 }
 
 DataSourceManager::~DataSourceManager() {
