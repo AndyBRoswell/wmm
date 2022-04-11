37831ba94876d551495417a6204ead09d60cf541
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-04-11 22:11:17 +0800

    Update QtTreeModel.cpp

--- wmm/src/QtTreeModel.cpp
+++ wmm/src/QtTreeModel.cpp
@@ -5,18 +5,64 @@ using lsize_t = QtTreeModel::lsize_t;
 
 /// QtTreeModel::TreeItem
 
+/**
+ * Initially, each node has no children. They're added using the InsertChildren() function.
+ * @param Data
+ * @param Parent
+ */
 QtTreeModel::TreeItem::TreeItem(const QList<QVariant>& Data, QtTreeModel::TreeItem* Parent) : ItemData(Data), ParentItem(Parent) {}
 
+// The destructor ensures that each child added to the item is deleted when the item itself is deleted.
 QtTreeModel::TreeItem::~TreeItem() { qDeleteAll(ChildItem); }
 
 QtTreeModel::TreeItem* QtTreeModel::TreeItem::Parent() { return ParentItem; }
 
+/**
+ * Returns a specific child from the internal list of children.
+ * @param Number The number of the required child of this node.
+ * @return
+ */
 QtTreeModel::TreeItem* QtTreeModel::TreeItem::Child(int Number) {
 //    if (Number < 0 || Number >= ChildItem.size()) return nullptr;
     return ChildItem[Number];
 }
 
+lsize_t QtTreeModel::TreeItem::ChildCount() const { return ChildItem.count(); }
 
+/**
+ * The ChildNumber() function is used to determine the index of the child in its parent's list of children.
+ * The root item has no parent item; for this item, we return 0 to be consistent with the other items.
+ * @return
+ */
+lsize_t QtTreeModel::TreeItem::ChildNumber() const { return ParentItem != nullptr ? ParentItem->ChildItem.indexOf(this) : 0; }
+
+/**
+ * Returns the number of elements stored at this tree node.
+ * @return
+ */
+lsize_t QtTreeModel::TreeItem::ColumnCount() const { return ItemData.count(); }
+
+/**
+ * Data is retrieved using the Data() function, which accesses the appropriate element in this tree node.
+ * @param Column
+ * @return
+ */
+QVariant QtTreeModel::TreeItem::Data(int Column) const {
+//    if (Column < 0 || Column >= ItemData.size()) return {};
+    return ItemData[Column];
+}
+
+/**
+ * Data is set using the SetData() function, which only stores values in this tree node for valid list indexes, corresponding to column values in the model.
+ * @param Column
+ * @param Value
+ * @return Whether the operation was succeeded.
+ */
+bool QtTreeModel::TreeItem::SetData(lsize_t Column, const QVariant& Value) {
+    if (Column < 0 || Column >= ItemData.size()) return false;
+    ItemData[Column] = Value;
+    return true;
+}
 
 /// QtTreeModel
 
