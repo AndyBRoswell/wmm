55d7e58cf18badcb65cf065e62898b3120899162
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-05-13 21:50:19 +0800

    Update setup.sh

--- wmm/setup.sh
+++ wmm/setup.sh
@@ -2,4 +2,11 @@
 # Running this script will DELETE all the dependencies downloaded, built and installed, and then REDOWNLOAD, REBUILD and REINSTALL them again.
 
 cwd=$PWD
-echo $cwd
+shsd=$cwd/$(dirname $0)
+prjd=$shsd
+${3rdd}=$shsd/3rd
+buildd=${3rdd}/build
+dld=${3rdd}/download
+instd=${3rdd}/install/GCC
+
+echo $cwd $shsd $prjd ${3rdd} $buildd $dld $instd
