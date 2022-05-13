33ce5b1e566a730c143b08f462e83fccb8d10b72
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-05-13 21:09:00 +0800

    Update setup.sh

--- wmm/setup.sh
+++ wmm/setup.sh
@@ -1,5 +1,5 @@
 # This script MUST be put at the root dir of this project.
 # Running this script will DELETE all the dependencies downloaded, built and installed, and then REDOWNLOAD, REBUILD and REINSTALL them again.
 
-$cwd = $pwd
+$cwd=$pwd
 echo $cwd
