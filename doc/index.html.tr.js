bc86ad115eff0c281fe3af163e97ca96d81352af
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-07-09 00:12:06 +0800

    /

--- doc/index.html.tr.js
+++ doc/index.html.tr.js
@@ -1,13 +1,10 @@
 const Dict = {
     "写作素材管理器": "Writing Materials Manager",
-    "架构图": "architecture diagram",
+    "架构图": "Architecture Diagram",
 }
 
-console.log('Translation started.')
 const ElementsTranslate = document.querySelectorAll('.tr')
-console.log('Translating ' + ElementsTranslate.length + ' elements ...')
 ElementsTranslate.forEach(e => {
     const TranslatedText = Dict[e.innerHTML]
     if (TranslatedText) e.innerHTML = TranslatedText
 })
-console.log('Translation completed.')
