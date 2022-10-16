c964e39d86a8cee65016eb4c821f0fe3e795275d
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-16 18:37:31 +0800

    /

--- test/gtest_main.cpp
+++ test/gtest_main.cpp
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
 
+#include <QByteArray>
+#include <QString>
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
+}
+
+// Tests of WMM begin here.
+TEST(Algorithm, StringIeq) { // ieq is from powershell
+
 }
