c9e08b9e02dc8f6f57a53d38506bdc46f9cd7ad7
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-16 20:55:14 +0800

    /

--- test/TinyRandom.h
+++ test/TinyRandom.h
@@ -5,15 +5,15 @@
 #include <random>
 
 namespace TinyRandom {
-    std::mt19937_64 default_random_engine(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::mt19937_64 random_engine(std::chrono::high_resolution_clock::now().time_since_epoch().count());
     std::uniform_int_distribution<uintmax_t> max_uniform_uint_dist(0, UINTMAX_MAX);
     std::uniform_int_distribution<intmax_t> max_uniform_int_dist(INTMAX_MIN, INTMAX_MAX);
     
     namespace Number {
         template<class T> T RanInt() {
             static_assert(std::is_arithmetic_v<T>, "Only built-in arithmetic types are allowed.");
-            if constexpr (std::is_signed_v<T>) return max_uniform_int_dist(default_random_engine);
-            else return max_uniform_uint_dist(default_random_engine);
+            if constexpr (std::is_signed_v<T>) return max_uniform_int_dist(random_engine);
+            else return max_uniform_uint_dist(random_engine);
         }
 
         // return a random integer between [a, b], b - a <= max(T)
@@ -21,18 +21,20 @@ namespace TinyRandom {
             static_assert(std::is_arithmetic_v<T>, "Only built-in arithmetic types are allowed.");
             const T L = b - a;
             if constexpr (std::is_signed_v<T>) {
-                const T x = max_uniform_int_dist(default_random_engine);
+                const T x = max_uniform_int_dist(random_engine);
                 return a + (x % L - INTMAX_MIN % L) % L;
             }
             else {
-                const T x = max_uniform_uint_dist(default_random_engine);
+                const T x = max_uniform_uint_dist(random_engine);
                 return a + x % L;
             }
         }
     }
 
     namespace String {
-
+        std::uniform_int_distribution<qsizetype> uniform_ucase_latin_dist('A', 'Z');
+        std::uniform_int_distribution<qsizetype> uniform_lcase_latin_dist('a', 'z');
+        std::uniform_int_distribution<qsizetype> uniform_dec_digit_dist('0', '9');
     }
 }
 
--- test/gtest_main.cpp
+++ test/gtest_main.cpp
@@ -19,12 +19,5 @@ TEST(HelloTest, BasicAssertions) {
 
 // Tests of WMM begin here.
 TEST(Algorithm, StringIeq) { // ieq is from powershell
-    std::mt19937_64 e(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-    
-    std::uniform_int_distribution<qsizetype> U_UCase('A', 'Z');
-    std::uniform_int_distribution<qsizetype> U_LCase('a', 'z');
-    std::uniform_int_distribution<qsizetype> U_Len(1, default_string_length);
-    {
 
-    }
 }
