2d0f9b176acd5f539d1093f26f744c1f9b2adab3
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-16 21:20:35 +0800

    Update TinyRandom.h

--- test/TinyRandom.h
+++ test/TinyRandom.h
@@ -11,14 +11,14 @@ namespace TinyRandom {
     
     namespace Number {
         template<class T> T RanInt() {
-            static_assert(std::is_arithmetic_v<T>, "Only built-in arithmetic types are allowed.");
+            static_assert(std::is_integral_v<T>, "Only built-in integral types are allowed.");
             if constexpr (std::is_signed_v<T>) return max_uniform_int_dist(random_engine);
             else return max_uniform_uint_dist(random_engine);
         }
 
         // return a random integer between [a, b], b - a <= max(T)
         template<class T> T RanInt(const T a, const T b) {
-            static_assert(std::is_arithmetic_v<T>, "Only built-in arithmetic types are allowed.");
+            static_assert(std::is_integral_v<T>, "Only built-in integral types are allowed.");
             const T L = b - a;
             if constexpr (std::is_signed_v<T>) {
                 const T x = max_uniform_int_dist(random_engine);
@@ -32,9 +32,14 @@ namespace TinyRandom {
     }
 
     namespace String {
-        std::uniform_int_distribution<qsizetype> uniform_ucase_latin_dist('A', 'Z');
-        std::uniform_int_distribution<qsizetype> uniform_lcase_latin_dist('a', 'z');
-        std::uniform_int_distribution<qsizetype> uniform_dec_digit_dist('0', '9');
+        std::uniform_int_distribution<intmax_t> uniform_ucase_latin_dist('A', 'Z');
+        std::uniform_int_distribution<intmax_t> uniform_lcase_latin_dist('a', 'z');
+        std::uniform_int_distribution<intmax_t> uniform_dec_digit_dist('0', '9');
+        std::uniform_int_distribution<intmax_t> uniform_visible_ascii_dist(0x20, 0x7E);
+    }
+
+    namespace Misc {
+        
     }
 }
 
