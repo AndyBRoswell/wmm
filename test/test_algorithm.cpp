c2e9331862c463d3827baa254b35ab178a669b50
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-22 12:05:06 +0800

    Update test_algorithm.cpp

--- test/test_algorithm.cpp
+++ test/test_algorithm.cpp
@@ -117,31 +117,34 @@ TEST(TestAlgorithm, Integer) {
         }
     }
     { // ASCII
-        using namespace tiny_random::chr;
+        using namespace tiny_random;
+        using tiny_random::chr::ASCII_char_type;
+        using tiny_random::chr::ASCII;
+        using tiny_random::chr::ASCII_string;
+        using enum ASCII_char_type;
 
         constexpr size_t n = 1e3; // test count
 
-        const std::set<int> shex(hex, hex + sizeof(hex) - 1);
-        const std::set<int> slhex(lhex, lhex + sizeof(hex) - 1);
-        const std::set<int> sualnum(ualnum, ualnum + sizeof(ualnum) - 1);
-        const std::set<int> slalnum(lalnum, lalnum + sizeof(lalnum) - 1);
-        const std::function<bool(int)> contains[] = {
-            [](int c) { return isdigit(c); },
-            [&shex](int c) { return shex.contains(c); },
-            [&slhex](int c) { return slhex.contains(c); },
-            [](int c) { return isupper(c); },
-            [](int c) { return islower(c); },
-            [](int c) { return isalpha(c); },
-            [&sualnum](int c) { return sualnum.contains(c); },
-            [&slalnum](int c) { return slalnum.contains(c); },
-            [](int c) { return isalnum(c); },
-            [](int c) { return ispunct(c); },
-            [](int c) { return c >= 0x20 and c <= 0x7e; },
+        const std::set<int> shex(chr::hex, chr::hex + sizeof(chr::hex) - 1);
+        const std::set<int> slhex(chr::lhex, chr::lhex + sizeof(chr::lhex) - 1);
+        const std::set<int> sualnum(chr::ualnum, chr::ualnum + sizeof(chr::ualnum) - 1);
+        const std::set<int> slalnum(chr::lalnum, chr::lalnum + sizeof(chr::lalnum) - 1);
+        const std::vector<ASCII_char_type> enum_to_test = { dec, hex, lhex, ucase, lcase, alpha, ualnum, lalnum, alnum, punct, printable, };
+        const std::vector<std::tuple<std::function<bool(int)>, std::set<int>>> f = {
+            { [](int c) { return isdigit(c); }, {}, },
+            { [&shex](int c) { return shex.contains(c); }, {}, },
+            { [&slhex](int c) { return slhex.contains(c); }, {}, },
+            { [](int c) { return isupper(c); }, {}, },
+            { [](int c) { return islower(c); }, {}, },
+            { [](int c) { return isalpha(c); }, {}, },
+            { [&sualnum](int c) { return sualnum.contains(c); }, {}, },
+            { [&slalnum](int c) { return slalnum.contains(c); }, {}, },
+            { [](int c) { return isalnum(c); }, {}, },
+            { [](int c) { return ispunct(c); }, {}, },
+            { [](int c) { return c >= 0x20 and c <= 0x7e; }, {}, },
         };
-        std::set<int> coverage = {};
 
-        using U = std::underlying_type_t<ASCII_char_type>;
-        for (U t = static_cast<U>(ASCII_char_type::dec); t <= static_cast<U>(ASCII_char_type::printable); ++t) {
+        for (const auto& i : enum_to_test) {
 
         }
     }
