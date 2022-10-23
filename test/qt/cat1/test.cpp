88880b31ae9137743674ae589f44dc6f3acb7c58
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-23 17:20:11 +0800

    Update test.cpp

--- test/qt/cat1/test.cpp
+++ test/qt/cat1/test.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 #include <QObject>
 #include <QTest>
 
@@ -14,10 +16,18 @@ private slots:
     }
 
     void TreeView() {
-        WritingMaterialsManager::TreeView TreeView;
+        WritingMaterialsManager::TreeView tree_view;
 
         // signals are emitted when this TreeView is focused
-
+        QTestEventList event_list;
+        constexpr Qt::MouseButton mouse_keys[] = { Qt::MouseButton::LeftButton, Qt::MouseButton::RightButton, Qt::MouseButton::MiddleButton };
+        for (const auto& m : mouse_keys) {
+            event_list.addMouseClick(m);
+            event_list.addMouseDClick(m);
+            event_list.addMousePress(m);
+            event_list.addMouseRelease(m);
+        }
+        event_list.simulate(&tree_view);
     }
 
     void cleanupTestCase() {
