87f2ec56f91241b676f821051650822763dae6ab
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-24 20:29:59 +0800

    Update Algorithm.cpp

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -1,14 +1,10 @@
 #include "Algorithm.h"
 
 namespace WritingMaterialsManager {
-/// struct CaseInsensitiveHasher
-
     size_t CaseInsensitiveHasher::operator()(const QByteArray& Str) const noexcept {
         return *reinterpret_cast<const size_t*>(QCryptographicHash::hash(Str.toUpper(), DefaultHashAlgorithm).right(sizeof(size_t)).constData());
     }
     size_t CaseInsensitiveHasher::operator()(const QString& Str) const noexcept {
         return *reinterpret_cast<const size_t*>(QCryptographicHash::hash(Str.toUpper().toUtf8(), DefaultHashAlgorithm).right(sizeof(size_t)).constData());
     }
-
-/// struct CaseInsensitiveStringComparator
 }
