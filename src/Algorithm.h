d3ddcd82821fdd63d4437b38b462fe5760cd4714
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-25 00:08:48 +0800

    /

--- src/Algorithm.h
+++ src/Algorithm.h
@@ -10,8 +10,8 @@ namespace WritingMaterialsManager {
         
         template<class T> consteval static bool is_UTF_8_compatible_charset_f() {
             return std::is_same_v<T, QByteArrayView> || std::is_same_v<T, QLatin1StringView> || std::is_same_v<T, QUtf8StringView>
-                // types with implicit conversions to the types above
-                || std::is_same_v<std::remove_cvref_t<T>, char*> || std::is_same_v<std::remove_cvref_t<T>, char8_t>;
+                // types which may have implicit conversions to the types above
+                || std::is_same_v<std::remove_cvref_t<T>, const char*> || std::is_same_v<std::remove_cvref_t<T>, const char8_t*>;
         }
         template<class T> struct is_UTF_8_compatible_charset : std::integral_constant<bool, is_UTF_8_compatible_charset_f<T>()> {};;
         template<class T> static constexpr bool is_UTF_8_compatible_charset_v = is_UTF_8_compatible_charset<T>::value;
@@ -21,27 +21,30 @@ namespace WritingMaterialsManager {
         size_t operator()(const QAnyStringView Str) const noexcept; // Qt recommends pass string views by value
         size_t operator()(const QStringView Str) const noexcept;
         template<class T = QByteArrayView> typename std::enable_if_t<is_UTF_8_compatible_charset_v<T>, size_t> operator()(const T Str) const noexcept {
-            return this->operator()(QByteArray(Str.constData(), Str.size()));
+            if constexpr (std::is_class_v<T>) { return this->operator()(QByteArray(Str.constData(), Str.size())); }
+            else { return this->operator()(QByteArray(Str)); }
         }
     };
 
     struct CaseInsensitiveStringComparator {
-        template<class T> consteval static bool has_static_compare_f() {
-            return std::is_same_v<T, QAnyStringView>
-                || std::is_same_v<T, QByteArrayView> || std::is_same_v<T, QLatin1StringView> || std::is_same_v<T, QStringView> || std::is_same_v<T, QUtf8StringView>
-                || std::is_same_v<T, QString>
-                // types with implicit conversions to the types above; hint by qt
-                || std::is_same_v<std::remove_cvref_t<T>, char*> || std::is_same_v<std::remove_cvref_t<T>, char8_t>
-                || std::is_same_v<std::remove_cvref_t<T>, char16_t> || std::is_same_v<std::remove_cvref_t<T>, uint16_t*> || std::is_same_v<std::remove_cvref_t<T>, QChar*>;
-        }
-        template<class T> struct has_static_compare : std::integral_constant<bool, has_static_compare_f<T>()> {};
-        template<class T> static constexpr bool has_static_compare_v = has_static_compare<T>::value;
+
         
-        template<class T = QAnyStringView> typename std::enable_if_t<has_static_compare_v<T>, bool> operator()(const T LHS, const T RHS) const noexcept { // Qt recommends pass string views by value
-            return T::compare(LHS, RHS, Qt::CaseInsensitive) == 0;
+        template<class T> consteval static bool supported_but_no_static_compare_f() {
+            return std::is_same_v<T, QByteArrayView> || std::is_same_v<T, QLatin1StringView> || std::is_same_v<T, QStringView> || std::is_same_v<T, QUtf8StringView>
+                // types which may have implicit conversions to the types above; hint by qt
+                || std::is_same_v<std::remove_cvref_t<T>, const char*> || std::is_same_v<std::remove_cvref_t<T>, const char8_t*>
+                || std::is_same_v<std::remove_cvref_t<T>, const char16_t*> || std::is_same_v<std::remove_cvref_t<T>, const uint16_t*> || std::is_same_v<std::remove_cvref_t<T>, const QChar*>;
         }
-        bool operator()(const QByteArray& LHS, const QByteArray& RHS) const noexcept {
-            return LHS.compare(RHS, Qt::CaseInsensitive) == 0;
+        template<class T> struct supported_but_no_static_compare : std::integral_constant<bool, supported_but_no_static_compare_f<T>()> {};
+        template<class T> static constexpr bool supported_but_no_static_compare_v = supported_but_no_static_compare<T>::value;
+        
+        bool operator()(const QAnyStringView LHS, const QAnyStringView RHS) const noexcept; // Qt recommends pass string views by value
+        bool operator()(const QString& LHS, const QString& RHS) const noexcept; // Qt recommends pass string views by value
+        template<class T = QByteArrayView> typename std::enable_if_t<supported_but_no_static_compare_v<T>, bool> operator()(const T LHS, const T RHS) const noexcept {
+            if constexpr (std::is_class_v<T>) { return LHS.compare(RHS, Qt::CaseInsensitive) == 0; }
+            else {
+                
+            }
         }
     };
 }
