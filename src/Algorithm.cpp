a3851e3475f9d14958f63c7099c7cd085d6f0022
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-24 22:05:55 +0800

    /

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -9,6 +9,10 @@ namespace WritingMaterialsManager {
         const QByteArrayView v(reinterpret_cast<const char*>(STR.constData()), STR.size() * 2);
         return *reinterpret_cast<const size_t*>(QCryptographicHash::hash(v, DefaultHashAlgorithm).right(sizeof(size_t)).constData());
     }
+    size_t CaseInsensitiveHasher::operator()(const QAnyStringView Str) const noexcept {
+        if (Str.size_bytes() / Str.size() == 1) { return this->operator()(QByteArray(static_cast<const char*>(Str.data()), Str.size_bytes())); }
+        else { return this->operator()(Str.toString()); }
+    }
     size_t CaseInsensitiveHasher::operator()(const QStringView Str) const noexcept {
         const QString STR = QString::fromRawData(Str.constData(), Str.size()).toUpper();
         const QByteArrayView v(reinterpret_cast<const char*>(STR.constData()), STR.size() * 2);
