33f3af60995a937663a9e93919dd5aabfb6d680b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-25 18:38:47 +0800

    Create datgen.cpp

---
+++ test/google/datgen.cpp
@@ -0,0 +1,11 @@
+// Qt
+#include <QFile>
+#include <QDir>
+#include <QJsonDocument>
+
+// googletest
+#include <gtest/gtest.h>
+
+TEST(TreeEditor, ConvertCharsetForArtificialData) {
+    const QDir wd("test/TreeEditor");
+}
