cc71e21885a7ee16197d6f1bc9bd8396e764cada
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-25 22:34:11 +0800

    Update test.cpp

--- test/qt/cat2/test.cpp
+++ test/qt/cat2/test.cpp
@@ -29,25 +29,24 @@ private slots:
         wmm::TreeEditor tree_editor;
 
         qRegisterMetaType<wmm::TreeEditor>();
-        std::unordered_map<QByteArray, QSignalSpy> signal_spy;
-        signal_spy.emplace("ShouldUpdatePathName()", QSignalSpy(&tree_editor, SIGNAL(ShouldUpdatePathName())));
-        signal_spy.emplace("ShouldUpdateFileType()", QSignalSpy(&tree_editor, SIGNAL(ShouldUpdateFileType())));
-        signal_spy.emplace("ShouldUpdateCharset()", QSignalSpy(&tree_editor, SIGNAL(ShouldUpdateCharset())));
+        QSignalSpy signal_spy_ShouldUpdatePathName(&tree_editor, SIGNAL(ShouldUpdatePathName()));
+        QSignalSpy signal_spy_ShouldUpdateFileType(&tree_editor, SIGNAL(ShouldUpdateFileType()));
+        QSignalSpy signal_spy_ShouldUpdateCharset(&tree_editor, SIGNAL(ShouldUpdateCharset()));
 
         const QDir wd("test/TreeEditor");
         const QStringList test_file_list = wd.entryList({ "*.json" });
 
         for (const auto& test_file_name : test_file_list) {
             const QStringList file_name_part = test_file_name.split("_");
-            const QByteArray charset = file_name_part.size() == 3 ? file_name_part[2].toUtf8().toUpper() : "UTF-8";
+            const QByteArray charset = file_name_part.size() == 3 ? file_name_part[2].toUtf8().toUpper() : QByteArray("UTF-8");
             //const QByteArray charset = file_name_part.size() == 3 ? file_name_part[2].toUtf8(): "utf-8";
 
             // test side
             tree_editor.SetCharset(charset);
-            QCOMPARE(signal_spy["ShouldUpdateCharset()"].count(), 1);
+            QCOMPARE(signal_spy_ShouldUpdateCharset.count(), 1);
             tree_editor.OpenFile(test_file_name);
-            QCOMPARE(signal_spy["ShouldUpdatePathName()"].count(), 1);
-            QCOMPARE(signal_spy["ShouldUpdateFileType()"].count(), 1);
+            QCOMPARE(signal_spy_ShouldUpdatePathName.count(), 1);
+            QCOMPARE(signal_spy_ShouldUpdateFileType.count(), 1);
 
             // verification side
             QFile test_file(test_file_name);
