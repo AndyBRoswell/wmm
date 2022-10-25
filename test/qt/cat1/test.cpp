92e2ed124b67299f0ba42fe6d7c08628fe1c4b05
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-25 15:59:29 +0800

    Update test.cpp

--- test/qt/cat1/test.cpp
+++ test/qt/cat1/test.cpp
@@ -11,68 +11,69 @@
 class test : public QObject {
     Q_OBJECT
 private:
+    inline static constexpr Qt::MouseButton mouse_keys[] = { Qt::MouseButton::LeftButton, Qt::MouseButton::RightButton, Qt::MouseButton::MiddleButton };
 
 private slots:
     void initTestCase() {
         qDebug("GUI Test Cat. 1"); // cat. 1 for relatively simpler GUI components
     }
 
-    void SignalAndCtor() {
+    void TreeView__basic() {
         namespace wmm = WritingMaterialsManager;
 
-        // signals are emitted when this TreeView is focused
+        wmm::TreeView tree_view;
+
+        // signals
+        qRegisterMetaType<wmm::TreeView>();
+        QSignalSpy spy(&tree_view, SIGNAL(MouseDown()));
+        for (const auto m : mouse_keys) {
+            QTest::mouseClick(tree_view.viewport(), m);
+            QTest::mouseDClick(tree_view.viewport(), m);
+            QTest::mousePress(tree_view.viewport(), m);
+            QTest::mouseRelease(tree_view.viewport(), m);
+        }
+        QCOMPARE(spy.count(), 6);
+    }
+
+    void TextField__basic() {
+        namespace wmm = WritingMaterialsManager;
+
+        wmm::TextField text_field;
+
+        // default settings
+        QCOMPARE(text_field.font(), wmm::DefaultCodeFont);
+
+        // signals
         QTestEventList events;
-        constexpr Qt::MouseButton mouse_keys[] = { Qt::MouseButton::LeftButton, Qt::MouseButton::RightButton, Qt::MouseButton::MiddleButton };
         for (const auto m : mouse_keys) {
             events.addMouseClick(m);
             events.addMouseDClick(m);
             events.addMousePress(m);
             events.addMouseRelease(m);
         }
-
-        // begin signal test
-        constexpr qsizetype signal_count = 6; // click, press
-
-        wmm::TreeView tree_view;
-        wmm::TextField text_field;
-        wmm::TextArea text_area;
-        {
-            qRegisterMetaType<wmm::TreeView>();
-            QSignalSpy spy(&tree_view, SIGNAL(MouseDown()));
-            for (const auto m : mouse_keys) {
-                QTest::mouseClick(tree_view.viewport(), m);
-                QTest::mouseDClick(tree_view.viewport(), m);
-                QTest::mousePress(tree_view.viewport(), m);
-                QTest::mouseRelease(tree_view.viewport(), m);
-            }
-            QCOMPARE(spy.count(), signal_count);
-        }
-        {
-            QSignalSpy spy(&text_field, &wmm::TextField::MouseDown);
-            events.simulate(&text_field);
-            QCOMPARE(spy.count(), signal_count);
-        }
-        {
-            qRegisterMetaType<wmm::TextArea>();
-            QSignalSpy spy(&text_area, SIGNAL(MouseDown()));
-            for (const auto m : mouse_keys) {
-                QTest::mouseClick(text_area.viewport(), m);
-                QTest::mouseDClick(text_area.viewport(), m);
-                QTest::mousePress(text_area.viewport(), m);
-                QTest::mouseRelease(text_area.viewport(), m);
-            }
-            QCOMPARE(spy.count(), signal_count);
-        }
-
-        // verify default font
-        QCOMPARE(text_field.font(), wmm::DefaultCodeFont);
-        QCOMPARE(text_area.font(), wmm::DefaultCodeFont);
+        QSignalSpy spy(&text_field, &wmm::TextField::MouseDown);
+        events.simulate(&text_field);
+        QCOMPARE(spy.count(), 6);
     }
 
-    void TreeView() {
-        std::unordered_map<QByteArray, std::function<void(void)>> test_function = {
-            
-        };
+    void TextArea__basic() {
+        namespace wmm = WritingMaterialsManager;
+
+        wmm::TextArea text_area;
+
+        // default settings
+        QCOMPARE(text_area.font(), wmm::DefaultCodeFont);
+
+        // signals
+        qRegisterMetaType<wmm::TextArea>();
+        QSignalSpy spy(&text_area, SIGNAL(MouseDown()));
+        for (const auto m : mouse_keys) {
+            QTest::mouseClick(text_area.viewport(), m);
+            QTest::mouseDClick(text_area.viewport(), m);
+            QTest::mousePress(text_area.viewport(), m);
+            QTest::mouseRelease(text_area.viewport(), m);
+        }
+        QCOMPARE(spy.count(), 6);
     }
 
     void cleanupTestCase() {
