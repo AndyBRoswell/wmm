209e7d59cf6aaf70b625b3f138c4fffda2749fbb
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-28 20:18:20 +0800

    /

---
+++ test/qt/cat3/test.cpp
@@ -0,0 +1,21 @@
+// Qt
+#include <QSignalSpy>
+#include <QTest>
+
+// files to be tested
+#include "src/MongoDBConsole.h"
+
+class test : public QObject {
+    Q_OBJECT
+private slots:
+    void initTestCase() {
+        qDebug("GUI Test Cat. 3");
+    }
+
+    void cleanupTestCase() {
+        qDebug("End GUI Test Cat. 2");
+    }
+};
+
+QTEST_MAIN(test)
+#include "test.moc"
