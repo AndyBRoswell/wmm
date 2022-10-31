51da4ad7070dc7fd76f5c89496a530ba8367250b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-10-31 12:50:08 +0800

    Update test.cpp

--- test/qt/cat3/test.cpp
+++ test/qt/cat3/test.cpp
@@ -12,6 +12,22 @@ private slots:
         qDebug("GUI Test Cat. 3");
     }
 
+    void MongoDBConsole__basic() {
+        namespace wmm = WritingMaterialsManager;
+
+        wmm::MongoDBConsole MongoDB_console;
+        //wmm::TreeEditor tree_editor;
+        //MongoDB_console.AddAssociatedEditor(&tree_editor);
+        { // show dbs
+            //MongoDB_console.ExecuteShellCommand();
+            //QSignalSpy spy(&MongoDB_console, &wmm::MongoDBConsole::NewShellCommand);
+            //QVERIFY(spy.isValid());
+        }
+        { // JSON operations
+
+        }
+    }
+
     void cleanupTestCase() {
         qDebug("End GUI Test Cat. 3");
     }
