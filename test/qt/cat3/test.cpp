83624405e71c5320a0abef522e888cab8cf570ce
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-11-03 15:06:08 +0800

    Update test.cpp

--- test/qt/cat3/test.cpp
+++ test/qt/cat3/test.cpp
@@ -52,9 +52,9 @@ private slots:
                 set, append,
             };
 
-            constexpr size_t nE = 100; // E = editor
-            constexpr size_t na = 100; // a = action
-            constexpr qsizetype lmax = 100;
+            constexpr size_t nE = 1e3; // E = editor
+            constexpr size_t na = 1e3; // a = action
+            constexpr qsizetype lmax = 1e3;
 
             wmm::DatabaseConsole database_console;
             std::vector<wmm::TreeEditor*> editor;
@@ -74,7 +74,7 @@ private slots:
                 case action::append: {
                     const auto s_before = editor[tiny_random::number::integer(0ull, nE - 1)]->RawView->toPlainText();
                     const auto s_extra = QString::fromStdString(tiny_random::chr::ASCII_string(tiny_random::number::integer(1ll, lmax)));
-                    const auto s_after = s_before + s_extra;
+                    const auto s_after = s_before + "\n" + s_extra;
                     database_console.AppendTextForAssociatedEditors(s_extra);
                     for (size_t i = 0; i < nE; ++i) { QCOMPARE(editor[i]->RawView->toPlainText(), s_after); }
                 } break;
